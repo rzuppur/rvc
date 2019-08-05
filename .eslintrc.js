e7e1298ba12a6cf244b0e632eccae6d040e0f44c
Reino Zuppur <reino.zuppur@gmail.com>
2019-08-05 11:20:10 +0300

    eslint conf

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,17 +2,45 @@ module.exports = {
   root: true,
   env: {
     node: true,
+    browser: true,
+    es6: true,
   },
   extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-    '@vue/typescript',
+    "plugin:vue/recommended",
+    "@vue/airbnb",
+    "@vue/typescript",
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    quotes: [2, "double"],
+    "no-param-reassign": 0,
+    "no-underscore-dangle": 0,
+    "vue/script-indent": [2, 2, {baseIndent: 1}],
+    "vue/require-default-prop": 0,
+    "newline-per-chained-call": 0,
+    "max-len": 0,
+    "camelcase": 0,
+    "indent": 0,
+    "array-bracket-spacing": 0,
+    "linebreak-style": 0,
+    "space-in-parens": 0,
+    "object-curly-newline": 0,
+    "no-unused-expressions": 0,
+    "lines-between-class-members": 0,
+    "no-plusplus": [1, { "allowForLoopAfterthoughts": true }],
+    "no-await-in-loop": 0,
+    "padded-blocks": 0,
   },
+
+  overrides: [
+    {
+      files: ["*.vue"],
+      rules: {
+        indent: "off",
+      },
+    },
+  ],
+
   parserOptions: {
-    parser: '@typescript-eslint/parser',
+    parser: "@typescript-eslint/parser",
   },
 };
