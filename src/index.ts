17f72ad95fe2d2f4a728c01e00f93f96984860b0
Reino Zuppur <reino@reinozuppur.com>
2019-10-03 02:01:51 +0300

    Create a method for mounting a component on root

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 // @ts-ignore
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { VueConstructor, PluginObject, Component } from "vue";
+import { Component, PluginObject, VueConstructor } from "vue";
 import Button from "./components/Button.vue";
 import Icon from "./components/Icon.vue";
 import Toast from "./components/Toast.vue";
@@ -14,6 +14,13 @@ declare global {
 
 const version = "__VERSION__";
 
+const mountComponentToRoot = (vue: VueConstructor, parent: any, componentConstructor: VueConstructor, propsData: object = {} ): Component => {
+  const node = document.createElement("div");
+  parent.$el.appendChild(node);
+  const Constructor = vue.extend(componentConstructor);
+  return <Component> new Constructor({ parent, propsData }).$mount(node);
+};
+
 const install = (Vue: VueConstructor): void => {
   Vue.component("r-button", Button);
   Vue.component("r-icon", Icon);
@@ -25,12 +32,8 @@ const install = (Vue: VueConstructor): void => {
     mounted() {
       // @ts-ignore
       if (!this.$parent) {
-        const node = document.createElement("div");
         // @ts-ignore
-        this.$el.appendChild(node);
-        const ToastConstructor = Vue.extend(Toast);
-        // @ts-ignore
-        toastComponent = new ToastConstructor({ parent: this }).$mount(node);
+        toastComponent = mountComponentToRoot(Vue, this, Toast);
       }
     },
   });
