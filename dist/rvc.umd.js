67eb827a59ffc7fc41d8c00bd108147cd55edc0a
Reino Zuppur <reino@reinozuppur.com>
2019-11-23 00:55:02 +0200

    media hide css raise specificity

--- dist/rvc.umd.js
+++ dist/rvc.umd.js
@@ -1,5 +1,5 @@
 /*!
- * rvc v0.42.0 
+ * rvc v0.42.1 
  * (c) 2019 Reino Zuppur
  * Released under the MIT License.
  */
@@ -2159,7 +2159,7 @@
     };
   };
 
-  var version = "0.42.0";
+  var version = "0.42.1";
 
   var mountComponentToRoot = function mountComponentToRoot(vue, parent, componentConstructor) {
     var propsData = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : {};
