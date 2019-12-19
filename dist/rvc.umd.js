928609d631311a8bc4dbb0d084cf3a998f12c64a
Reino Zuppur <reino@reinozuppur.com>
2019-12-19 15:15:23 +0200

    0.46.1 - contextual alternatives

--- dist/rvc.umd.js
+++ dist/rvc.umd.js
@@ -1,5 +1,5 @@
 /*!
- * rvc v0.46.0 
+ * rvc v0.46.1 
  * (c) 2019 Reino Zuppur
  * Released under the MIT License.
  */
@@ -2163,7 +2163,7 @@
     };
   };
 
-  var version = "0.46.0";
+  var version = "0.46.1";
 
   var mountComponentToRoot = function mountComponentToRoot(vue, parent, componentConstructor) {
     var propsData = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : {};
